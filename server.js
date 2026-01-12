46df64a4350ef8e488201458886deede1b91bb60
arcosum <123695409+tinglypuma4@users.noreply.github.com>
2026-01-12 11:11:24 -0600

    fixes

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir archivos estáticos
-app.use(express.static(__dirname));
-
 // Ruta principal - Menú de cartas
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -21,15 +18,9 @@ app.get('/mes-3', (req, res) => {
     res.sendFile(path.join(__dirname, 'aniversario-3-meses.html'));
 });
 
-// Aquí puedes agregar más cartas fácilmente:
-// app.get('/mes-4', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'aniversario-4-meses.html'));
-// });
+// Servir archivos estáticos (DESPUÉS de las rutas específicas)
+app.use(express.static(__dirname));
 
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
-    console.log(`\nRutas disponibles:`);
-    console.log(`  - http://localhost:${PORT}/          (Menú principal)`);
-    console.log(`  - http://localhost:${PORT}/mes-2     (Carta 2 meses)`);
-    console.log(`  - http://localhost:${PORT}/mes-3     (Carta 3 meses)`);
 });
